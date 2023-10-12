f75ab6114a957b6e3ece2d97d6b971554dc58227
Klaudine <109423321+klaudine@users.noreply.github.com>
2023-10-12 19:34:41 +0000

    made changes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector;
+  ".card".classList.add("heart");
+  setTimeout(() => {
+    document.querySelector;
+    ".card".classList.remove("heart");
+    document.querySelector;
+    ".card".classList.add("spade");
+  }, 1000);
 };
+
+let generateRandomNumber(){
+  let numb = Math.floor(Math.random() * 12);
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["diamond", "spade", "heart", "club"];
+}
